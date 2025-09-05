ab7ebdfdf29dff66e880c9d9e240da637a1bb8c5
Whitehathud <whitehathud@gmail.com>
2025-09-05 11:52:29 +0800

    added config.py

---
+++ backend/core/config.py
@@ -0,0 +1,48 @@
+from typing import List
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import field_validator
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    
+    # Application
+    APP_NAME: str = "Recipe Tech Backend"
+    APP_VERSION: str = "1.0.0"
+    DEBUG: bool = False
+    
+    # Security
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    
+    # Admin
+    ADMIN_USERNAME: str
+    ADMIN_PASSWORD: str
+    
+    # Database
+    DATABASE_URL: str = "sqlite:///./app.db"
+    
+    # CORS
+    CORS_ORIGINS: List[str] = ["http://localhost:3000, http://localhost:8000, http://localhost:8080"]
+    
+    # API
+    API_PREFIX: str = ""
+    DOCS_URL: str = "/docs"
+    REDOC_URL: str = "/redoc"
+    
+    @field_validator("CORS_ORIGINS", mode="before")
+    @classmethod
+    def parse_cors_origins(cls, v):
+        if isinstance(v, str):
+            return [origin.strip() for origin in v.split(",")]
+        return v
+    
+    #S3 Bucket
+    # AWS S3 Settings
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_REGION: str
+    S3_BUCKET_NAME: str
+    
+
+settings = Settings()
